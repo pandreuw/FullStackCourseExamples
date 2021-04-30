ae772cb0148694a0eed5236c9b86ea2a02ff7082
Gerardo Moreno <wanderup.allego@gmail.com>
2021-04-30 06:32:18 -0400

    20210430 first repo of the example portion

---
+++ Part1/partc/partc/src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />,
+  document.getElementById('root'))
+
+
+
+
+// let counter = 1
+
+// const refresh=()=>{
+//   ReactDOM.render(<App counter={counter}/>,
+//   document.getElementById('root'))
+
+// }
+
+// setInterval(()=>{
+//   refresh()
+//   counter+=1
+// }, 1000)
+
