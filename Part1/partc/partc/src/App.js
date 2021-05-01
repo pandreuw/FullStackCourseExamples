df51a311a47eb474499993b24f30f64531eaf143
Gerardo Moreno <wanderup.allego@gmail.com>
2021-05-01 18:03:50 -0400

    20210501 1803h Stateful component example rendering component.

--- Part1/partc/partc/src/App.js
+++ Part1/partc/partc/src/App.js
@@ -1,41 +1,24 @@
-import React, {useState} from 'react'
-
-
-//Primera parte de la subsección del tema
-
-const Hello = ({ name, age }) => {
-// no funcionó const date para obtener la fecha y la hora deseada
-// proxima vez investigar mas de esto .
-//   const Date = () => new Date() 
-
-const bornYear = () => new Date().getFullYear() -age
-
-
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>so you were probably born in {bornYear()} </p>
-      {/* <p>Fecha: {Date}</p>
-      <p>Hello {name}, espero que tengas un excelente dia!.</p>
-      <p>Te amo, eres el amor de mi vida.</p>
-      <p>Sólo quería que lo supieras de una manera diferente.</p>
-      <p>😘</p> */}
-    </div>
-  )
-}
+import React, { useState } from 'react'
 
 const App = () => {
-  const name = 'Gerardo'
-  const age = 38
+  const [counter, setCounter] = useState(0)
 
+
+  console.log("Before running setTimeout")
+  console.log({ counter })
+  console.log({ setCounter })
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log("After running setTimeout")
+  console.log({ counter })
+  console.log({ setCounter })
   return (
-    <div>
-      <h1>Saludos</h1>
-      <Hello name="Maya" age={26+10}/>
-      <Hello name="Leslie" age={37}/>
-      <Hello name="Gerardo" age={age}/>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-export default App;
+export default App
--- Part1/partc/partc/src/index.js
+++ Part1/partc/partc/src/index.js
@@ -1,22 +1,5 @@
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />,
-  document.getElementById('root'))
-
-
-
-
-// let counter = 1
-
-// const refresh=()=>{
-//   ReactDOM.render(<App counter={counter}/>,
-//   document.getElementById('root'))
-
-// }
-
-// setInterval(()=>{
-//   refresh()
-//   counter+=1
-// }, 1000)
-
+ReactDOM.render(<App />, 
+document.getElementById('root'))
